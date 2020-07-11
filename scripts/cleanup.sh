8d498dfb3e1d6d309299c0abeb0fe82d5b78705d
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-11 03:57:43 +0000

    Update cleanup to deal with projects that have stuck finalizers
    use -p to specify the project name base to remote
    
    use -f to force remove a stuck project (e.g. stuck in Terminating)

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,13 +1,68 @@
 #!/bin/bash
-
 declare PROJECT_BASE="fraud-demo"
+declare FORCE=""
+
+while (( "$#" )); do
+  case "$1" in
+    -p|--project-prefix)
+      PROJECT_BASE=$2
+      shift 2
+      ;;
+    -f|--force)
+      FORCE="true"
+      shift 1
+      ;;
+    -*|--*)
+      err "Error: Unsupported flag $1"
+      ;;
+    *) 
+      break
+  esac
+done
+
+# Assumes proxy has been setup
+force-clean() {
+    declare NAMESPACE=$1
+
+    echo "Force removing project $NAMESPACE"
+
+    oc get namespace $NAMESPACE -o json | jq '.spec = {"finalizers":[]}' > /tmp/temp.json
+    curl -k -H "Content-Type: application/json" -X PUT --data-binary @/tmp/temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+    rm /tmp/temp.json
+}
 
 # declare an array
 arrSuffix=( "dev" "stage" "cicd" )
  
+PROXY_PID=""
+if [[ ! -z "$FORCE" ]]; then
+    echo -n "opening proxy"
+
+    oc proxy &
+    PROXY_PID=$!
+fi
+
 # for loop that iterates over each element in arr
 for i in "${arrSuffix[@]}"
 do
     echo "Deleting $i"
-    oc delete project "${PROJECT_BASE}-${i}"
+    declare PROJECT="${PROJECT_BASE}-${i}"
+
+    if [[ -z "$(oc get project ${PROJECT} 2>/dev/null)" ]]; then
+        echo "Project $PROJECT already removed"
+        continue
+    fi
+
+    # actually delete the project
+    oc delete project $PROJECT
+
+    if [[ ! -z "$FORCE" ]]; then
+        force-clean $PROJECT
+    fi
 done
+
+if [[ ! -z "$PROXY_PID" ]]; then
+    echo "closing proxy"
+    kill $PROXY_PID
+fi
+

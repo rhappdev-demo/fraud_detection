a27258e389dbf3ea6d3aba0f3173d82b25e9b96d
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-14 12:14:19 +0000

    Recreate script can deal with file names that have spaces

--- scripts/util-recreate-pvc.sh
+++ scripts/util-recreate-pvc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PVC_FILE=$1
+PVC_FILE="$1"
 if [[ -z "$PVC_FILE" ]]; then
     echo "Must provide the name of a PVC.yaml"
     exit 1
 fi
 
-declare -r PVC_FILE_PATH=${DEMO_HOME}/kube/tekton/pipelines/${PVC_FILE}
+declare -r PVC_FILE_PATH="${DEMO_HOME}/kube/tekton/pipelines/${PVC_FILE}"
 
-declare -r PVC_NAME=$(cat ${PVC_FILE_PATH} | grep "name: "| cut -f 4 -d " ")
+declare -r PVC_NAME=$(cat "${PVC_FILE_PATH}" | grep "name: "| cut -f 4 -d " ")
 if [[ -z "$PVC_NAME" ]]; then
     echo "Could not find a PVC name in ${PVC_FILE_PATH}"
     exit 1
@@ -18,7 +18,7 @@ echo "Deleting pods that have mounted $PVC_NAME in current project"
 oc delete pods $(oc describe pvc/$PVC_NAME | grep "Mounted By" -A40 | sed "s/ //ig" | sed "s/MountedBy://ig")
 
 echo "Deleting the pvc"
-oc delete -f ${PVC_FILE_PATH}
+oc delete -f "${PVC_FILE_PATH}"
 
 echo "Recreating the pvc"
-oc create -f ${PVC_FILE_PATH}
+oc create -f "${PVC_FILE_PATH}"

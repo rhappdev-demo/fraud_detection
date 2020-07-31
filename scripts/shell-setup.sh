4e6f4e6dab7b72dc01d13de349a8954150dd71bc
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-31 08:00:42 +0000

    Small fix to shell-setup to more reliably set the DEMO_HOME variable
    Now uses BASH_SOURCE instead of $0 which doesn't always work under source
    
    Updates readme to use the '.' shorthand for 'source'

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export DEMO_HOME=$( cd "$(dirname "$0")/.." ; pwd -P )
+# per the following $0 doesn't work reliably when the script is sourced:
+# https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source
+export DEMO_HOME=$( cd "$(dirname "${BASH_SOURCE}")/.." ; pwd -P )
+
+echo "DEMO_HOME set to $DEMO_HOME"
 
 alias cpr='tkn pr cancel $(tkn pr list -o name --limit 1 | cut -f 2 -d "/")'
 alias ctr='tkn tr cancel $(tkn tr list -o name --limit 1 | cut -f 2 -d "/")'
